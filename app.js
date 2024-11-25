bba255b0cded745f3e00cefabd73b2991e78af2d
thewickest <graubuntu@gmail.com>
2024-11-25 18:42:56 +0100

    Track data into file

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 
 app.use(express.json());
 
-// /test
-app.get('/test', async (req, res) => {
-    res.status(200).send({ data: 'test' });
+// /track
+app.post('/track', async (req, res) => {
+    const data = req.body;
+
+    fs.appendFile('./data.json', JSON.stringify(data) + '\n', (err) => {
+        if (err) {
+            return res.status(500).send({ message: 'Error while processsing the data' });
+        }
+    });
+
+    res.status(200).send(data);
 });
+
 
 module.exports = app;
